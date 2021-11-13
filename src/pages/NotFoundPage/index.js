5f51c0dfbbdb1c82f684e3a0f60cc9f8e2d17746
LDan22 <dan.lupu.dl@gmail.com>
2021-11-13 12:27:52 +0000

    API, BLL layer

---
+++ src/pages/NotFoundPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export function NotFoundPage() {
+  return (
+    <>
+      <div>
+        <div>
+          4
+          <span role="img" aria-label="Crying Face">
+                    😢
+          </span>
+          4
+        </div>
+        <p>Page not found.</p>
+      </div>
+    </>
+  )
+}
