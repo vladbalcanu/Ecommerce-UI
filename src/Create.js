134b75ac787a498682a7b628e1ad7b1e64e3323f
VLAD PC <vlad.andrei.balcanu@gmail.com>
2021-11-15 10:13:35 +0200

    AddProduct campuri

--- src/Create.js
+++ src/Create.js
@@ -1,24 +1,28 @@
 import { useState } from "react";
 import {useHistory } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const Create = () => {
     const [title,setTitle]=useState('');
     const [description,setDescription]=useState('');
-    const [producer,setProducer]=useState('Benq');
+    const [producer,setProducer]=useState('');
     const [price,setPrice]=useState('');
     const [product_code,setProductCode]=useState('');
     const [color,setColor]=useState('');
-    const [warranty,setWarranty]=useState('300');
-    const [year,setYear]=useState('2021');
-    const [category,setCategory]=useState('1');
-    // const [image,setImage]=useState('');
+    const [warranty,setWarranty]=useState('');
+    const [year,setYear]=useState('');
+    const [category,setCategory]=useState('');
+    const {data}=useFetch('http://127.0.0.1:8000/api/v1/catalogue/category/');
+    const categories=data?.results;
+    //const [image,setImage]=useState('');
     
     const [isPending,setIsPending]=useState(false);
     const history = useHistory();
+    console.log({categories})
 
     const handleSubmit= (e) => {
         e.preventDefault();
-        const product= {title,product_code,color,warranty,producer,price,description,year,category };
+        const product= {title,product_code,color,warranty,producer,price,description,year,category};
 
         setIsPending(true);
         fetch('http://127.0.0.1:8000/api/v1/catalogue/product/',{
@@ -31,8 +35,6 @@ const Create = () => {
             setIsPending(false);
             history.push('/');
         })
-
-    
     }
     
     return ( 
@@ -63,17 +65,38 @@ const Create = () => {
                 <textarea
                 required
                 value={color}
-                onChange={(e)=>setColor(e.target.value)}
-                />
+                onChange={(e)=>setColor(e.target.value)}/>
+                <label>Product warranty:</label>
+                <textarea
+                required
+                value={warranty}
+                onChange={(e)=>setWarranty(e.target.value)}/>
+                <label>Product year:</label>
+                <textarea
+                required
+                value={year}
+                onChange={(e)=>setYear(e.target.value)}/>
+                <label> Product Category:</label>
+                <select value={category}
+                onChange={(e)=>setCategory(e.target.value)}>
+                    {categories && categories.map(item => (
+
+                        <option value={item.id}>{item.title}
+                        </option>
+                    ))}
+                    </select>
                 <label> Product Producer:</label>
-                <select value={producer}
-                onChange={(e)=> setProducer(e.target.value)}>
-                    <option value="BenQ">BenQ</option>
-                    <option value="Eizo">Eizo</option>
-                </select>
+                <textarea
+                required
+                value={producer}
+                onChange={(e)=>setProducer(e.target.value)}/>
+                {/* <label> Product image:</label>
+                <input type="image"
+                value={image}
+                onChange={(e)=>setImage(e.target.value)}
+                /> */}
                 {!isPending && <button>Add Product</button> }
                 {isPending && <button>Adding Product...</button> }
-                
             </form>
         </div>
      );
