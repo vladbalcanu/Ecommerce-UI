3b4a7d0c582942c159806688b0f06f06448fb5c6
VLAD PC <vlad.andrei.balcanu@gmail.com>
2021-12-09 18:31:13 +0200

    productCard

--- src/pages/ProductsPage/index.js
+++ src/pages/ProductsPage/index.js
@@ -1,105 +1,26 @@
-<<<<<<< HEAD
-import * as React from 'react'
-import {useEffect, useState} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {selectProducts} from '../../store/catalogue/selectors'
-import {getProducts, searchProducts} from '../../store/catalogue/thunks'
-import styles from './ProductsPage.module.css'
-import {selectCategories} from '../../store/categories/selectors'
-import {getCategories} from '../../store/categories/thunks'
-import RecipeReviewCard from '../../components/productCard'
-import Grid from '@mui/material/Grid'
-
-
-export const ProductsPage = () => {
-  const [text, setText] = useState('')
-  const [category, setCategory] = useState('')
-  const dispatch = useDispatch()
-  const products = useSelector(selectProducts)
-  const categories = useSelector(selectCategories)
-
-  useEffect(() => {
-    dispatch(getProducts())
-    dispatch(getCategories())
-
-  }, [dispatch])
-
-  function onChangeSearch(event) {
-    setText(event.target.value)
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault()
-    console.log(text)
-    dispatch(searchProducts(text, category))
-
-  }
-
-
-  return (
-    <div className={styles.productList}>
-
-      <div className={styles.searchBar}>
-        <form
-          action="/"
-          method="get"
-          onSubmit={handleSubmit}
-        >
-
-          <label htmlFor="header-search">
-          </label>
-          <input
-            type="text"
-            id="header-search"
-            placeholder="Search Products"
-            name="searchbar"
-            onChange={onChangeSearch}
-          />
-          <button onClick={handleSubmit} type="submit">Search</button>
-          <label>
-            {<select value={category}
-                     onChange={(e) => setCategory(e.target.value)} className={styles.categorySearch}>
-              {categories && categories.map(item => (
-
-                <option value={item.id}>{item.title}
-                </option>
-              ))}
-            </select>
-            }
-          </label>
-
-        </form>
-      </div>
-
-
-      <Grid container spacing={2} columns={12}>
-
-        {products.map(item => (
-
-          <Grid item xs={4}>
-            <style>
-              text-decoration:none;
-            </style>
-
-            <RecipeReviewCard product={item}/>
-
-          </Grid>
-        ))}
-      </Grid>
-    </div>
-  )
-=======
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectProducts } from "../../store/catalogue/selectors";
-import { getProducts, } from "../../store/catalogue/thunks";
+import { getProducts, searchProducts, } from "../../store/catalogue/thunks";
 import styles from './ProductsPage.module.css';
 import {selectCategories} from "../../store/categories/selectors";
 import {getCategories} from "../../store/categories/thunks";
 import CategoryService from "../../services/catalogue/CategoryService";
+import RecipeReviewCard from "../../components/productCard";
+import { InsertEmoticonTwoTone } from "@mui/icons-material";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 export const ProductsPage = () => {
     const [text,setText] = useState("")
@@ -113,12 +34,14 @@ export const ProductsPage = () => {
     }, [])
     
     function onChangeSearch(event){
-        setText(event.target.value)
-        console.log(text);
+        setText(event.target.value);
     }
 
     function handleSubmit(event){
         event.preventDefault();
+        console.log(text);
+        dispatch(searchProducts(text,category))
+        
     }
 
     useEffect(()=>{
@@ -146,7 +69,7 @@ export const ProductsPage = () => {
                 name="searchbar"
                 onChange = {onChangeSearch} 
             />
-                <button type="submit">Search</button>
+                <button onClick={handleSubmit} type="submit">Search</button>
                 <label >
                 { <select value={category}
                 onChange={(e)=>setCategory(e.target.value)} className={styles.categorySearch}>
@@ -164,23 +87,21 @@ export const ProductsPage = () => {
             
 
 
-            {  
-                products.filter(item => text === "" || item.title.includes(text)).map(item =>
-                    <Link to={`/catalogue/searchProducts/products/${item.id}`} className={styles.linkStyle}>
-                    <div className={styles.productPreview}>
-                        <h2>Name: {item.title}</h2>
-                        <h3>Producer: {item.producer}</h3>
-                        <h4>Price: {item.price}</h4>
-                        <img className={styles.productPhoto} src={item.image}>
-                            </img>
+              <Grid container spacing={2} columns={12}>
+
+                { products.map(item => (
+                    
+                    <Grid item xs={4}>
+                        <style>
+                            text-decoration:none;
+                            </style>
                         
+                            <RecipeReviewCard product={item}/>
                         
-                    </div>
-                    </Link>
-                )
-            }
+                    </Grid>
+                ))}
+                </Grid>
             </div>
-    );
->>>>>>> b7db1ca... Feature:catalogue DAL , store UI
+    )
 }
 
--- src/store/catalogue/thunks.js
+++ src/store/catalogue/thunks.js
@@ -13,3 +13,16 @@ export const getProducts=() => async (dispatch) =>{
         dispatch(setIsPending(false))
     }
 }
+
+export const searchProducts=(search,categoryId) => async (dispatch) =>{
+    dispatch(setIsPending(true))
+    try{
+        console.log("thunk");
+        const paginatedProducts=await ProductsService.searchProducts(search,categoryId)
+        dispatch(setProducts({products:paginatedProducts.results}))
+    }catch(error){
+        dispatch(setError())
+    }finally{
+        dispatch(setIsPending(false))
+    }
+}
