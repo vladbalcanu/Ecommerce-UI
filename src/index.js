36ceaeae22befa3bcbc7c3879010cda4f0d01378
LDan22 <dan.lupu.dl@gmail.com>
2021-12-10 10:33:35 +0200

    SignIn/SignUp ui

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import './index.css';
+
 
 import {configureAppStore} from './store/configureStore'
 import {Provider} from 'react-redux'
